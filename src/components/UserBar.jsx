5808d9e99dda9779d10d3d252c85687e7c721ffa
Erdum <erdumadnan@gmail.com>
2022-09-27 17:14:04 +0500

    Fix avatar not loading

--- src/components/UserBar.jsx
+++ src/components/UserBar.jsx
@@ -3,7 +3,7 @@ import { Flex, Avatar, Text } from "@chakra-ui/react";
 const UserBar = ({ avatar, username }) => {
 	return (
 		<Flex w="100%" h="16" bg="gray.100" px="4" justifyContent="start" alignItems="center">
-			<Avatar src={avatar} />
+			<Avatar referrerPolicy="no-referrer" src={avatar} />
 			<Text ml="2" fontWeight="semibold">{username ?? "Username"}</Text>
 		</Flex>
 	);
