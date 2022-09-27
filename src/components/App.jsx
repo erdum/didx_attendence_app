271f250470f0f8abb3afe6ab45273023131b02da
Erdum <erdumadnan@gmail.com>
2022-09-27 14:22:16 +0500

    Add home page initialized

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import { ChakraProvider, extendTheme, Flex, Image, Heading, Text } from "@chakra-ui/react";
+
+const App = () => {
+	const theme = extendTheme({
+		fonts: {
+			heading: `Quicksand`,
+			body: `Quicksand`,
+		}
+	});
+
+	return (
+		<ChakraProvider theme={theme}>
+			<Flex
+				w="100%"
+				h="12"
+				alignItems="stretch"
+				justifyContent="space-between"
+				bg="white"
+				boxShadow="base"
+				px="4"
+			>
+				<Image src="/logo.png" objectFit="cover" />
+				<Heading as="h1" size="md" alignSelf="center" fontWeight="semibold" color="blackAlpha.800">
+					Attendence Portal
+				</Heading>
+			</Flex>
+			<Flex
+				w="100%"
+				h="16"
+				alignItems="stretch"
+				justifyContent="center"
+				position="fixed"
+				bottom="0"
+				px="4"
+			>
+				<Text alignSelf="center" fontSize="sm">&#169; didx.net {new Date().getFullYear()}</Text>
+			</Flex>
+		</ChakraProvider>
+	);
+};
+
+export default App;
+ 
