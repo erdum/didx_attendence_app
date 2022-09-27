9c7b7fd25acb62594737ae806553b698896f7480
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-09-27 22:35:31 +0500

    Update states and logic lifted

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,10 @@
-import { useState } from "react";
-
 import { Box, Flex, Button } from "@chakra-ui/react";
 import { CheckIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 import UserBar from "./UserBar";
 import DateBar from "./DateBar";
 import DataBar from "./DataBar";
 
-import { autoSignIn, signIn } from "../utils/auth";
-import { doCheckIn, doCheckOut } from "../utils/flow";
-
-const Card = () => {
-	const [user, setUser] = useState(null);
-	autoSignIn((loggedUser) => setUser(loggedUser));
-
-	const handleCheckIn = () => {
-		if (user) {
-			doCheckIn(user);
-		} else {
-			signIn((loggedUser) => setUser(loggedUser));
-		}
-	};
-
-	const handleCheckOut = () => {
-		if (user) {
-			doCheckOut(user);
-		} else {
-			signIn((loggedUser) => setUser(loggedUser));
-		}
-	};
-
+const Card = ({ username, avatar, checkinTime, checkoutTime }) => {
 	return (
 		<Box
 			w="92%"
@@ -46,12 +22,12 @@ const Card = () => {
 			flexDirection="column"
 			py="4"
 		>
-			<UserBar avatar={user?.photoURL} username={user?.displayName}/>
+			<UserBar avatar={avatar} username={username} />
 			<DateBar />
 			<DataBar />
 			<Flex mt="auto" alignItems="center" justifyContent="space-around" py="1">
 				<Button
-					onClick={handleCheckIn}
+					onClick={() => handleFlow({ type: "check-in" })}
 					fontSize="sm"
 					bg="green.500"
 					color="white"
@@ -60,7 +36,7 @@ const Card = () => {
 					Check In <CheckIcon ml="2" />
 				</Button>
 				<Button
-					onClick={handleCheckOut}
+					onClick={() => handleFlow({ type: "check-out" })}
 					fontSize="sm"
 					bg="red.500"
 					color="white"
