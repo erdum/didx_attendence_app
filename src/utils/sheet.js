a526d1273ad63c3e6ef21493424aa9033388f409
Erdum <erdumadnan@gmail.com>
2022-09-28 12:12:39 +0500

    Add sheet utility for sheetson api

--- src/utils/sheet.js
+++ src/utils/sheet.js
@@ -1,5 +1,38 @@
-const getReq = async (url) => {};
+const Sheet = () => {
+	
+	const data = {};
 
-const postReq = async (data, url) => {};
+	const init = ({ apiKey, sheetId, sheetName }) => {
+		data.apiKey = apiKey;
+		data.sheetId = sheetId;
+		data.url = `https://api.sheetson.com/v2/sheets/${sheetName}`;
+	};
 
-const updateRow = async (data) => {};
+	const getReq = async (url) => {};
+
+	const postReq = async (data, url) => {
+		const req = await fetch(url, {
+			headers: {
+				"Authorization": `Bearer ${data.apiKey}`,
+				"X-Spreadsheet-Id": data.sheetId,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data)
+		});
+
+		const res = await req.json();
+		return res;
+	};
+
+	const updateRow = async (data) => {
+		const test = await postReq(data, data.url);
+		console.log(test);
+	};
+
+	return {
+		init,
+		updateRow,
+	};
+};
+
+export default Sheet;
