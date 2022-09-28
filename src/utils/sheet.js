cb62a310249bee2f58956860b4d8f7e397856ce7
Erdum <erdumadnan@gmail.com>
2022-09-28 15:34:26 +0500

    Update sheet interface build

--- src/utils/sheet.js
+++ src/utils/sheet.js
@@ -1,5 +1,4 @@
 const Sheet = () => {
-	
 	const data = {};
 
 	const init = ({ apiKey, sheetId, sheetName }) => {
@@ -8,30 +7,59 @@ const Sheet = () => {
 		data.url = `https://api.sheetson.com/v2/sheets/${sheetName}`;
 	};
 
-	const getReq = async (url) => {};
-
-	const postReq = async (data, url) => {
+	const getReq = async (url) => {
 		const req = await fetch(url, {
 			headers: {
-				"Authorization": `Bearer ${data.apiKey}`,
+				Authorization: `Bearer ${data.apiKey}`,
 				"X-Spreadsheet-Id": data.sheetId,
-				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(data)
 		});
 
 		const res = await req.json();
 		return res;
 	};
 
-	const updateRow = async (data) => {
-		const test = await postReq(data, data.url);
-		console.log(test);
+	const postReq = async (body, url) => {
+		const req = await fetch(url, {
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${data.apiKey}`,
+				"X-Spreadsheet-Id": data.sheetId,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
+		});
+
+		const res = await req.json();
+		return res;
+	};
+
+	const addRow = async (body, callback) => {
+		const result = await postReq(body, data.url);
+		callback(result);
+	};
+
+	const getAllRows = async (callback) => {
+		const { results } = await getReq(data.url);
+		callback(results);
+	};
+
+	const getRow = async (column, value, callback) => {
+		let rows;
+		await getAllRows((data) => rows = data);
+
+		const match = rows.find(elem => {
+			return elem && elem.hasOwnProperty(column) && Object.values(elem).includes(value);
+		});
+
+		callback(match ?? null);
 	};
 
 	return {
 		init,
-		updateRow,
+		addRow,
+		getAllRows,
+		getRow,
 	};
 };
 
