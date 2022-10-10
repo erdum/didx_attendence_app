715a1f0818e9873ff59edf263744a30df6e4a934
Erdum <erdumadnan@gmail.com>
2022-10-10 16:44:15 +0500

    Update hard coded values removed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,8 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYz8ZL0ef_zm23d0JUeJcyFkRytMVyhH0",
-  authDomain: "didx-attendance-portal-fa701.firebaseapp.com",
-  projectId: "didx-attendance-portal-fa701",
-  storageBucket: "didx-attendance-portal-fa701.appspot.com",
-  messagingSenderId: "1031404753998",
-  appId: "1:1031404753998:web:416e888f1ec08dcfcadf1f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
 };
 const app = initializeApp(firebaseConfig);
 
