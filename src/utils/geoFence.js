f1ff3857df4cb26ca92ed0594ded3ba7158a2b3d
Erdum <erdumadnan@gmail.com>
2022-10-13 14:58:36 +0500

    Update check multiple zones

--- src/utils/geoFence.js
+++ src/utils/geoFence.js
@@ -19,15 +19,13 @@ const GeoFence = () => {
 		);
 	};
 
-	const setGeoFenceCircle = (lat, long, radius) => {
-		data.geoFenceCircleLat = lat;
-		data.geoFenceCircleLong = long;
-		data.geoFenceCircleRadius = radius;
+	const setGeoFenceCircle = (circles) => {
+		data.circles = circles;
 	};
 
-	const isUserInFence = () => {
-		const centerLat = data.geoFenceCircleLat / (180 / Math.PI);
-		const centerLong = data.geoFenceCircleLong / (180 / Math.PI);
+	const checkFence = (lat, long, radius) => {
+		const centerLat = lat / (180 / Math.PI);
+		const centerLong = long / (180 / Math.PI);
 		const currentLat = data.lat / (180 / Math.PI);
 		const currentLong = data.long / (180 / Math.PI);
 
@@ -37,13 +35,18 @@ const GeoFence = () => {
 		let c = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(centerLat) * Math.cos(currentLat) * Math.pow(Math.sin(dlong / 2), 2);
 		c = 2 * Math.asin(Math.sqrt(c));
 		c = 6371 * c;
-		return c <= data.geoFenceCircleRadius;
+		return c <= radius;
+	};
+
+	const isUserInsideFence = () => {
+		const areaName = data.circles.find(({ lat, long, radius }) => checkFence(lat, long, radius));
+		return areaName?.name ?? false;
 	};
 
 	return {
 		getLocation,
 		setGeoFenceCircle,
-		isUserInFence,
+		isUserInsideFence,
 	};
 };
 
