0566c91ba4b8787cfee8f425fee52043088cbf31
Erdum <erdumadnan@gmail.com>
2022-10-18 17:58:54 +0500

    Update abstraction, complexity reduced

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,10 @@
 import { useState } from "react";
 
-import { Box, Flex, Button, ScaleFade } from "@chakra-ui/react";
-import { CheckIcon, ArrowForwardIcon } from "@chakra-ui/icons";
-import UserBar from "./UserBar";
-import DateBar from "./DateBar";
-import DataBar from "./DataBar";
+import { Box, ScaleFade } from "@chakra-ui/react";
 import UsersList from "./UsersList";
+import AttendancePanel from "./AttendancePanel";
 
-const Card = ({
-	username,
-	avatar,
-	checkinTime,
-	checkoutTime,
-	flowHandler,
-	userLoader,
-	checkinLoader,
-	checkoutLoader,
-	location,
-}) => {
+const Card = (props) => {
 	const [showList, setShowList] = useState(false);
 
 	return (
@@ -50,64 +37,7 @@ const Card = ({
 					flexDirection="column"
 					overflowY="auto"
 				>
-					{showList ? (
-						<UsersList />
-					) : (
-						<>
-							<UserBar
-								avatar={avatar}
-								username={username}
-								loader={userLoader}
-							/>
-							<DateBar />
-							<DataBar
-								checkinTime={checkinTime}
-								checkoutTime={checkoutTime}
-								checkinLoader={checkinLoader}
-								checkoutLoader={checkoutLoader}
-								location={location}
-							/>
-							<Button onClick={() => setShowList(true)} size="sm" w="30%" ml="6" mt="1">see others</Button>
-							<Flex
-								mt="auto"
-								alignItems="center"
-								justifyContent="space-around"
-								py="1"
-							>
-								<Button
-									isLoading={checkinLoader}
-									onClick={() => flowHandler({ type: "check-in" })}
-									fontSize="sm"
-									bg="green.500"
-									color="white"
-									px="5"
-									_active={{
-										background: "green.500",
-									}}
-									_hover={{
-										background: "green.500",
-									}}
-								>
-									Check In <CheckIcon ml="2" />
-								</Button>
-								<Button
-									isLoading={checkoutLoader}
-									onClick={() => flowHandler({ type: "check-out" })}
-									fontSize="sm"
-									bg="red.500"
-									color="white"
-									_active={{
-										background: "red.500",
-									}}
-									_hover={{
-										background: "red.500",
-									}}
-								>
-									Check out <ArrowForwardIcon ml="2" />
-								</Button>
-							</Flex>
-						</>
-					)}
+					{showList ? <UsersList /> : <AttendancePanel {...props} showList={setShowList} />}
 				</Box>
 			</ScaleFade>
 		</Box>
