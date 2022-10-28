b1ed6fdc1026b6370fc425ffe8b8f1500b0b6783
Erdum <erdumadnan@gmail.com>
2022-10-28 17:49:11 +0500

    Update live date update implemented

--- src/components/DateBar.jsx
+++ src/components/DateBar.jsx
@@ -1,7 +1,14 @@
+import { useState, useEffect } from "react";
 import { Flex, Text } from "@chakra-ui/react";
 
 const DateBar = () => {
-	const date = new Date();
+	const [date, setDate] = useState(new Date);
+
+	useEffect(() => {
+		const interval = setInterval(() => setDate(new Date), 1000);
+
+		return () => clearInterval(interval);
+	}, []);
 
 	return (
 		<Flex
