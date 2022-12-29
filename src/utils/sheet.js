bb2b4e4efe52a73e68e5ac991eef69d0d883bd7d
Erdum <erdumadnan@gmail.com>
2022-12-29 17:09:17 +0500

    Update custom API integrated

--- src/utils/sheet.js
+++ src/utils/sheet.js
@@ -3,21 +3,16 @@ import GSheetReader from "g-sheets-api-erdum";
 const Sheet = () => {
 	const data = {};
 
-	const init = ({ apiKey, sheetId, sheetName, sheetsonApiKey }) => {
+	const init = ({ apiKey }) => {
 		data.apiKey = apiKey;
-		data.sheetId = sheetId;
-		data.sheetsonApiKey = sheetsonApiKey;
-		data.sheetName = sheetName;
-		data.url = new URL(`https://api.sheetson.com/v2/sheets/${sheetName}`);
+		data.url = new URL("162.243.33.19/attendance");
 	};
 
 	const getReq = async (url) => {
 		try {
-			const URL = url.searchParams.append("limit", 100);
 			const req = await fetch(url, {
 				headers: {
-					Authorization: `Bearer ${data.sheetsonApiKey}`,
-					"X-Spreadsheet-Id": data.sheetId,
+					apikey: data.apiKey,
 				},
 			});
 
@@ -33,8 +28,7 @@ const Sheet = () => {
 			const req = await fetch(url, {
 				method: "POST",
 				headers: {
-					Authorization: `Bearer ${data.sheetsonApiKey}`,
-					"X-Spreadsheet-Id": data.sheetId,
+					apikey: data.apiKey,
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify(body),
@@ -47,77 +41,26 @@ const Sheet = () => {
 		}
 	};
 
-	const patchReq = async (body, url, row) => {
-		try {
-			const req = await fetch(`${url}/${row}`, {
-				method: "PATCH",
-				headers: {
-					Authorization: `Bearer ${data.sheetsonApiKey}`,
-					"X-Spreadsheet-Id": data.sheetId,
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(body),
-			});
-
-			const res = await req.json();
-			return res;
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	const addRow = async (body, callback) => {
+	const markAttendance = async (body, callback) => {
 		const result = await postReq(body, data.url);
-		callback && callback(result);
+		callback && callback(result ?? null);
 	};
 
-	const getRow = async (column, value, callback) => {
-		const payload = {};
-		payload[column] = value;
-
-		filterByColumn(payload, (rows) => {
-			const result = rows.at(-1);
-			callback && callback(result ?? null);
-		});
+	const markCheckout = async (uid, body, callback) => {
+		const result = await postReq(body, `${data.url}/${uid}`);
+		callback && callback(result ?? null);
 	};
 
-	const updateRow = async (body, column, value, callback) => {
-		getRow(column, value, async ({ rowIndex: id }) => {
-			// +2 offset for id beacuase spreadsheet counts from 1 and 1 row is occupied by the headers
-			const result = await patchReq(body, data.url, id + 2);
-			callback && callback(result ?? null);
-		});
-	};
-
-	const filterByColumn = (columns, callback) => {
-		const options = {
-			apiKey: data.apiKey,
-			sheetId: data.sheetId,
-			sheetName: data.sheetName,
-			returnAllResults: false,
-			filter: columns,
-			filterOptions: {
-				operator: "or",
-				matching: "loose",
-			},
-		};
-		GSheetReader(
-			options,
-			(results) => callback(results),
-			(error) => {
-				throw new Error(
-					`Error while fetching data with GSheetReader: ${error}`
-				);
-			}
-		);
+	const getTodayAttendance = async (date) => {
+		const result = await getReq(`${data.url}/today/${date}`);
+		callback && callback(result ?? null);
 	};
 
 	return {
 		init,
-		addRow,
-		updateRow,
-		getRow,
-		filterByColumn,
+		markAttendance,
+		markCheckout,
+		getTodayAttendance,
 	};
 };
 
